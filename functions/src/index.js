1ae11bd1c0b96a1e45769ead5c6acf5038687d3f
Preet Vachhani <147071335+Skyro26@users.noreply.github.com>
2025-08-17 22:33:06 +0400

    Delete functions directory

--- functions/src/index.js
+++
@@ -1,8 +0,0 @@
-const functions = require("firebase-functions");
-const chat = require("./chat");
-const notes = require("./notes");
-const trips = require("./trips");
-
-exports.chat = chat.chatHandler;
-exports.notes = notes.notesHandler;
-exports.trips = trips.tripsHandler;
